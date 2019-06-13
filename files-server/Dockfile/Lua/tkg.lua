c8dbe569125ede05c480b4003673ccc98805c639
tinker <whoszus@yahoo.com>
2019-06-13 09:31:11 +0800

    updated: 文件路径可配置，参数内排除文件本身；

--- files-server/Dockfile/Lua/tkg.lua
+++ files-server/Dockfile/Lua/tkg.lua
@@ -1,5 +1,4 @@
 local tkg = {}
-
 -- 是否在队列中
 -- 返回位置
 function tkg.in_array(v, tab)
@@ -96,14 +95,12 @@ function tkg.split(mainString, delim)
  
     local start = 1
     local t = {}
-
     while true do
         ngx.log(ngx.ERR,type(mainString))
         local pos = string.find (mainString, delim, start, true) -- plain find
         if not pos then
             break
         end
- 
         table.insert (t, string.sub (mainString, start, pos - 1))
         start = pos + string.len (delim)
     end
@@ -113,10 +110,64 @@ function tkg.split(mainString, delim)
     return t
 end
 
+-- 另外一个split方法 实现方式的
+function tkg.tsplit(inputstr,sep)
+    -- ngx.log(ngx.ERR,type(inputstr))
+    if sep == nil then
+            sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+            table.insert(t, str)
+    end
+    return t
+end
+
+-- table 转成 string
+function tkg.table_to_string(tbl)
+    local result = "{"
+    for k, v in pairs(tbl) do
+        -- Check the key type (ignore any numerical keys - assume its an array)
+        if type(k) == "string" then
+            result = result.."[\""..k.."\"]".."="
+        end
+
+        -- Check the value type
+        if type(v) == "table" then
+            result = result..table_to_string(v)
+        elseif type(v) == "boolean" then
+            result = result..tostring(v)
+        else
+            result = result.."\""..v.."\""
+        end
+        result = result..","
+    end
+    -- Remove leading commas from the result
+    if result ~= "" then
+        result = result:sub(1, result:len()-1)
+    end
+    return result.."}"
+end
+
+
 
 -- 获取form-data的请求参数需要用到upload库来获取表单，这个库安装openresty默认已经带了，也可以上GITHUB下载最新版本
 -- args = tkg:post_form_data(form, err)
+
+-- read: ["header",["Content-Disposition","form-data; name=\"file1\"; filename=\"a.txt\"","Content-Disposition: form-data; name=\"file1\"; filename=\"a.txt\""]]
+-- read: ["header",["Content-Type","text\/plain","Content-Type: text\/plain"]]
+-- read: ["body","Hello"]
+-- read: ["body",", wor"]
+-- read: ["body","ld"]
+-- read: ["part_end"]
+-- read: ["header",["Content-Disposition","form-data; name=\"test\"","Content-Disposition: form-data; name=\"test\""]]
+-- read: ["body","value"]
+-- read: ["body","\r\n"]
+-- read: ["part_end"]
+-- read: ["eof"]
+-- read: ["eof"]z
 function tkg.post_form_data(form,err)
+    local cjson = require "cjson"
 
     if not form then
         ngx.log(ngx.ERR, "failed to new upload: ", err)
@@ -127,6 +178,7 @@ function tkg.post_form_data(form,err)
     local paramTable = {["s"]=1}
     local tempkey = ""
     while true do
+        local i =1 ;
         local typ, res, err = form:read()
         if not typ then
             ngx.log(ngx.ERR, "failed to read: ", err)
@@ -135,24 +187,31 @@ function tkg.post_form_data(form,err)
         local key = ""
         local value = ""
         if typ == "header" then
-            ngx.log(ngx.ERR,type(res[2]))
-            local res_2 =res[2] 
-            local key_res = tkg:split(res_2,";")
-            -- key_res = key_res[2]
-            -- ngx.log(ngx.ERR,type(key_res))
-            -- key_res = tkg:split(key_res,"=")
-            -- key = (string.gsub(key_res[2],"\"",""))
-            -- paramTable[key] = ""
-            -- tempkey = key
+            local key_res = tkg.tsplit(res[2],";")
+            ngx.log(ngx.ERR,cjson.encode({"key_res",key_res}))
+            local key_name = key_res[2]
+            if type(key_name) ~= 'table' and key_name ~= nil then 
+                key_res = tkg.tsplit(key_name,"=")
+                if key[2] ~= nil  and type(key[2]) ~= 'table' then 
+                    -- goto continue
+                    key = (string.gsub(key_res[2],"\"",""))
+                    paramTable[key] = ""
+                    tempkey = key
+                end
+            end
+            
         end
         if typ == "body" then
             value = res
             if paramTable.s ~= nil then paramTable.s = nil end
             paramTable[tempkey] = value
+            ngx.log(ngx.ERR,cjson.encode({"body",value}))
+
         end
         if typ == "eof" then
             break
         end
+        -- ::continue::
     end
     return paramTable
 end
@@ -181,4 +240,78 @@ function tkg.new()
     return args
 end
 
+
+
+function tkg.loadFormInput (form)
+    local stringy = require "stringy" 
+    local result = {
+        data = {},
+        indexes = {}
+    }
+
+    -- Should be set to 4096 or 8192 for real-world settings
+
+    if not form then
+        -- There is no form data to read from
+        return nil
+    end
+
+
+    local part_index = 1
+    local part_name, part_value
+
+    while true do
+        local typ, res, err = form:read()
+        if not typ then
+            return nil -- An error happened, 'failed to read'
+        end
+
+        if typ == "header" then
+            if stringy.startswith(string.lower(res[1]), "content-disposition") then
+                local parts = stringy.split(res[3], ";")
+                local current_parts = stringy.split(stringy.strip(parts[2]), "=")
+                if string.lower(table.remove(current_parts, 1)) == "name" then
+                    local current_value = stringy.strip(table.remove(current_parts, 1))
+                    part_name = string.sub(current_value, 2, string.len(current_value) - 1)
+                end
+            end
+        elseif typ == "body" then
+            part_value = res
+        elseif typ == "part_end" then
+            if part_name ~= nil and part_name ~= "file_entity" then
+                result.data[part_index] = {
+                    name = part_name,
+                    value = part_value
+                }
+
+                result.indexes[part_name] = part_index
+
+                -- Reset fields for the next part
+                part_value = nil
+                part_name = nil
+                part_index = part_index + 1
+            end
+        elseif typ == "eof" then
+            -- finish reading the input
+            break
+        else
+            -- do nothing
+        end
+    end
+
+    return result
+end
+
+function tkg.getParam(form,param_name)
+    if form ==nil or param_name ==nil then 
+        return nil;
+    end
+    local args = tkg.loadFormInput(form)
+    local indexes = args.indexes;
+    local param_index = indexes[param_name];
+    if param_index ~= nil  then 
+        return args.data[param_index].value;
+    end
+    return nil;
+end
 return tkg
--- files-server/Dockfile/Lua/upload-resty.lua
+++ files-server/Dockfile/Lua/upload-resty.lua
@@ -8,10 +8,22 @@ local conf = {fileHome='/home/tinker/temp/upload/',version=1007,allow_exts={'mpe
 
 
 
+local function getFileSecondPath()
+    local file_path  = tkg.getParam(form,"file_path")
+    local 
+    if file_path ~= nil then 
+        return file_path
+    end
+    return os.date('%Y')..os.date('%m')..os.date('%d')..'/';
+end
+
+
+
+
 local function uploadHandle()
     -- ngx.log(ngx.ERR,"测试tkg~~")
     -- ngx.log(ngx.ERR,tkg)
-    local resposeData={code=500,msg='upload fail',desc='上传失败',server_verison= conf.version,file_orign_name= orign_name_array,diskstatus={diskuse=tkg:getdiskuse(),diskspace= tkg:getdiskspace()}}
+    local resposeData={code=500,msg='upload fail',desc='上传失败',server_verison= conf.version,file_orign_name= orign_name_array,diskstatus={diskuse=tkg.getdiskuse(),diskspace= tkg.getdiskspace()}}
     local file
     local file_name
     local file_name_arry = {}
@@ -23,94 +35,95 @@ local function uploadHandle()
     
     -- test code 
     -- local typ, res, err = form:read()
-    ngx.say(cjson.encode(tkg.post_form_data(form,err)))
+    -- ngx.say(cjson.encode(tkg.loadFormInput(form,"file_path")))
     -- testcode end 
 
     
-    -- while true do
-    --     form:set_timeout(1000)
-    --     local typ, res, err = form:read() 
-    --     if not typ then
-    --         ngx.say(cjson.encode({code=503, msg='failed to read',desc='读取form失败!'}))
-    --         ngx.log(ngx.DEBUG,cjson.encode({typ, res}));
-    --         return
-    --     end
-    --     -- local form_args = tkg.post_form_data(from,err)
+    while true do
+       
+        form:set_timeout(1000)
+        local typ, res, err = form:read() 
+        if not typ then
+            ngx.say(cjson.encode({code=503, msg='failed to read',desc='读取form失败!'}))
+            ngx.log(ngx.DEBUG,cjson.encode({typ, res}));
+            return
+        end
+        -- local form_args = tkg.post_form_data(from,err)
         
-    --     if typ == "header" then
-    --         if res[1] == "Content-Disposition" then
-    --             filename = tkg:getFilename(res[2])
-    --             if filename then
-    --                 local extension = tkg:get_extension(filename)
-    --                 if not extension then
-    --                     -- todo 修改为标准格式；
-    --                     ngx.say(cjson.encode({code=501, msg='无法获取文件后缀', desc=res}))
-    --                     return 
-    --                 end
+        if typ == "header" then
+            if res[1] == "Content-Disposition" then
+                filename = tkg.getFilename(res[2])
+                if filename then
+                    local extension = tkg.get_extension(filename)
+                    if not extension then
+                        -- todo 修改为标准格式；
+                        ngx.say(cjson.encode({code=501, msg='无法获取文件后缀', desc=res}))
+                        return 
+                    end
 
-    --                 -- 创建文件路径
-    --                 local dir = conf.fileHome..os.date('%Y')..os.date('%m')..os.date('%d')..'/'   
-    --                 local status = os.execute('mkdir -p '..dir)
-    --                 if not status then
-    --                     -- TODO 修改为标准格式
-    --                     ngx.say(cjson.encode({code=501, msg='创建目录失败'}))
-    --                     return
-    --                 end  
+                    -- 创建文件路径
+                    local dir = conf.fileHome..getFileSecondPath()  
+                    local status = os.execute('mkdir -p '..dir)
+                    if not status then
+                        -- TODO 修改为标准格式
+                        ngx.say(cjson.encode({code=501, msg='创建目录失败'}))
+                        return
+                    end  
                     
-    --                 -- 如果文件扩展名命中-- 使用文件原来的名字
-    --                 if tkg:in_array(extension, conf.allow_exts) then
-    --                     local file,err=io.open(dir..filename)
-    --                     if file == nil then 
-    --                         ngx.log(ngx.ERR,"ERR....filename: "..filename)
-    --                         file_name = dir..tkg:getFileRealName(filename)
-    --                         else
-    --                         -- 文件名重复处理 时间戳_文件名
-    --                         file.close()
-    --                         file_name = dir..os.time().."_"..tkg:getFileRealName(filename)
-    --                         end
-    --                 else
-    --                     local file_id = tkg:uuid2()
-    --                     file_name = dir..file_id.."."..extension
-    --                 end
+                    -- 如果文件扩展名命中-- 使用文件原来的名字
+                    if tkg.in_array(extension, conf.allow_exts) then
+                        local file,err=io.open(dir..filename)
+                        if file == nil then 
+                            ngx.log(ngx.ERR,"ERR....filename: "..filename)
+                            file_name = dir..tkg.getFileRealName(filename)
+                            else
+                            -- 文件名重复处理 时间戳_文件名
+                            file.close()
+                            file_name = dir..os.time().."_"..tkg.getFileRealName(filename)
+                            end
+                    else
+                        local file_id = tkg.uuid2()
+                        file_name = dir..file_id.."."..extension
+                    end
                     
-    --                 -- 处理文件名返回处理
-    --                 if file_name then
-    --                     file = io.open(file_name, "w+")
-    --                     if not file then
-    --                         -- TODO 修改为标准格式
-    --                         ngx.say(cjson.encode({code=500, msg='failed to write file',imgurl=''}))
-    --                         return
-    --                     else
-    --                         file_name = string.gsub(file_name, conf.fileHome, '')
-    --                         table.insert(file_name_arry,file_name);
-    --                         table.insert(orign_name_array,filename);
-    --                     end
-    --                 end
-    --             end
-    --         end
-    --     elseif typ == "body" then
-    --         if file then
-    --             file:write(res)            
-    --         end
-    --     elseif typ == "part_end" then
-    --         if file then
-    --             file:close()
-    --             file = nil
-    --         end
-    --     elseif typ == "eof" then
-    --         if file_name then 
-    --             --todo  修改返回值 
-    --             resposeData.code=200;
-    --             ngx.say(cjson.encode(resposeData));
-    --             ngx.log(ngx.DEBUG,'file_name' .. file_name)
-    --         else
-    --             ngx.say(cjson.encode({code=509, msg='form name do not existed',desc='',server_verison= conf.version,file_orign_name= orign_name_array,diskstatus= {diskuse= tkg:getdiskuse(),diskspace= tkg:getdiskspace()}}))
-    --         end
-    --         -- 跳出while
-    --         break
-    --     else
-    --     end
-    -- end
+                    -- 处理文件名返回处理
+                    if file_name then
+                        file = io.open(file_name, "w+")
+                        if not file then
+                            -- TODO 修改为标准格式
+                            ngx.say(cjson.encode({code=500, msg='failed to write file',imgurl=''}))
+                            return
+                        else
+                            file_name = string.gsub(file_name, conf.fileHome, '')
+                            table.insert(file_name_arry,file_name);
+                            table.insert(orign_name_array,filename);
+                        end
+                    end
+                end
+            end
+        elseif typ == "body" then
+            if file then
+                file:write(res)            
+            end
+        elseif typ == "part_end" then
+            if file then
+                file:close()
+                file = nil
+            end
+        elseif typ == "eof" then
+            if file_name then 
+                --todo  修改返回值 
+                resposeData.code=200;
+                ngx.say(cjson.encode(resposeData));
+                ngx.log(ngx.DEBUG,'file_name' .. file_name)
+            else
+                ngx.say(cjson.encode({code=509, msg='form name do not existed',desc='',server_verison= conf.version,file_orign_name= orign_name_array,diskstatus= {diskuse= tkg.getdiskuse(),diskspace= tkg.getdiskspace()}}))
+            end
+            -- 跳出while
+            break
+        else
+        end
+    end
 end
 -- ngx.say(cjson.encode(_M:uploadHandle()))
 uploadHandle()
